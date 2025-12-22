09885c23f49892a9ce87290b10ccdc6d2b3d9e1d
Muhammad Mutasem <muhammad.mutasem.m@gmail.com>
2025-12-23 00:10:44 +0400

    upload sql files

---
+++ project_sql/5_optimal_skills.sql
@@ -0,0 +1,41 @@
+with skills_demand as (
+SELECT  skills_dim.skill_id,
+skills_dim.skills,
+
+count(skills_job_dim.skill_id) as demand_count
+ FROM job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE 
+job_title_short = 'Data Analyst' AND 
+job_work_from_home = true
+AND salary_year_avg is NOT NULL
+GROUP BY skills_dim.skill_id,
+skills_dim.skills
+
+
+), average_salary as (    
+SELECT  skills_dim.skill_id,
+
+Round( AVG (salary_year_avg),0) as avg_salary
+ FROM job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE 
+job_title_short = 'Data Analyst' AND salary_year_avg is NOT NULL
+  AND job_work_from_home = true
+
+GROUP BY skills_dim.skill_id
+)
+
+SELECT skills_demand.skill_id,
+skills_demand.skills,
+demand_count,
+avg_salary
+
+FROM skills_demand
+INNER JOIN average_salary ON skills_demand.skill_id = average_salary.skill_id 
+WHERE demand_count >10
+ORDER BY avg_salary DESC, demand_count DESC
+
+LIMIT 25;
